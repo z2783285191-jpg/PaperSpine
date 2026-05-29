5d7190acb828ecf465e7b1f3a1b349e4fb7b4447
Wubin2023 <148868755+Wubin2023@users.noreply.github.com>
2026-05-29 12:07:29 +0800

    Remove accidentally tracked .venv directory

--- paper-spine-promo-video/.venv/Lib/site-packages/dlinfo/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = ['DLInfo']
-
-import sys
-
-# pylint: disable=import-private-name; internal
-if sys.platform == 'darwin':
-    from dlinfo._macosx import DLInfo
-else:
-    from dlinfo._glibc import DLInfo
--- paper-spine-promo-video/.venv/Lib/site-packages/language_tags/__init__.py
+++
@@ -1,1 +0,0 @@
-from language_tags.tags import tags
